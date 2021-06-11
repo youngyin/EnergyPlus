7dae8c1167244ff5b850301696c04982c59833a3
youngyin <wjswjs6879@naver.com>
2021-06-12 00:44:26 +0900

    delete unused api

--- connect-rds/main.js
+++ connect-rds/main.js
@@ -126,14 +126,6 @@ app.get('/userSearch', (req, res) =>{
 
 })
 
-app.get('/api/get/user', (req, res) =>{
-
-  connection.query(`SELECT * FROM ${userDB}`, (err, rows, fields) =>{
-    res.send(rows)
-  });
-
-})
-
 app.get('/recordSearch', (req, res) =>{
 
   if(req.query.searchText != ''){
@@ -171,7 +163,7 @@ app.get('/collectorSearch', (req, res) =>{
   }
 
 })
-
+/*
 // 2. 사용자 아이디로 기록 검색하기
 app.get('/idSearch', (req, res, next)=>{
 
@@ -190,107 +182,7 @@ app.get('/idSearch', (req, res, next)=>{
 
   }
 
-})
-
-// 회원가입
-app.post('/api/post/user', function(req,res){  
-  var inputData;
-  req.on('data', (data) => {
-    inputData = JSON.parse(data);
-  });
-  req.on('end', () => {
-    console.log(
-      `user_id : ${inputData.user_id}, 
-      user_pw : ${inputData.user_pw}, 
-      phone : ${inputData.phone}` );
-    
-      connection.query(
-      `INSERT INTO ${userDB} (user_id, user_pw, phone) 
-      values ("${inputData.user_id}", "${inputData.user_pw}", "${inputData.phone}")`
-      
-      , (err, rows, fields) => {
-        res.send(err); // 쿼리 수행 완료!
-    });
-  });
-});
-
-// 기록 남기기
-app.post('/api/post/record', function(req,res){  
-  var inputData;
-  req.on('data', (data) => {
-    inputData = JSON.parse(data);
-  });
-  req.on('end', () => {
-    console.log(
-      `user_id : ${inputData.user_id},
-      volume : ${inputData.volume}, 
-      discharge_date : ${getToday()}, 
-      inputData.collector_id : ${inputData.collector_id}` );
-
-    connection.query(
-      `INSERT INTO ${recordDB} (user_id, volume, discharge_date, collector_id) 
-      values ("${inputData.user_id}", "${inputData.volume}", 
-      "${getToday()}", "${inputData.collector_id}")`
-      
-      , (err, rows, fields) => {
-        res.send(err); // 쿼리 수행 완료!
-    });
-  });
-});
-
-// 수거 기기 등록하기
-app.post('/api/post/collector', function(req,res){  
-  var inputData;
-  req.on('data', (data) => {
-    inputData = JSON.parse(data);
-  });
-  req.on('end', () => {
-    console.log(
-      `location : ${inputData.location}` );
-    
-      connection.query(
-      `INSERT INTO ${collectorDB} (location) 
-      values ("${inputData.location}")`
-      
-      , (err, rows, fields) => {
-        res.send(err); // 쿼리 수행 완료!
-    });
-  });
-});
-
-// 아이디로 수거기 정보 업데이트
-app.put('/api/put/collector/required/:collector_id', function(req,res){  
-  var inputData;
-  req.on('data', (data) => {
-    inputData = JSON.parse(data);
-  });
-  req.on('end', () => {
-    console.log("required : "+inputData.required);
-
-    connection.query(
-      `update ${collectorDB} set required = ${inputData.required}
-      where collector_id = ${req.params.collector_id}`
-      
-      , (err, rows, fields) => {
-        res.send("ok!"); // 결과를 출력합니다!
-    });
-  });
-});
-
-// 회원 탈퇴
-app.delete('/api/delete/user', function(req,res){
-  var inputData;
-  req.on('data', (data) => {
-    inputData = JSON.parse(data);
-  });
-  req.on('end', () => {
-    console.log(`user_id : ${inputData.user_id}`);
-    connection.query(`delete from ${userDB} where user_id = "${inputData.user_id}"`
-      , (err, rows, fields) => {
-        res.send(err); // 쿼리 수행 완료!
-    });
-  });
-});
+})*/
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
